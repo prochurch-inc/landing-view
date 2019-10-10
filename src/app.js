80c22fd6ca22a6c103e97a49dd97f6ba8ce68729
Chirag Parekh <chiragparekhn@gmail.com>
2019-10-10 14:17:28 +0530

    package.json entry file changed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
 import Vue from 'vue'
 import LandingPreview from './components/landing'
 
-export default {
-    install(Vue, options) {
-        Vue.component('landing-preview', LandingPreview)
-    }
-}
+exports.LandingPreview = LandingPreview
