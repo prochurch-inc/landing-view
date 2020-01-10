0a1be1e426f9bb2d578e487945467ca22270332d
Lucian B <brodos@outlook.com>
2020-01-10 17:04:44 -0600

    oh well...

--- src/components/form/phone.vue
+++ src/components/form/phone.vue
@@ -3,7 +3,6 @@
         <h2 class="text-2xl mb-6 text-center">{{inputs[0].title}}</h2>
         <div class="flex items-center justify-center my-6">
             <the-mask :mask="'(###) ###-####'" v-model="phoneModel" class="py-3 px-6 border text-center rounded-sm"/>
-            <span class="text-xs text-red-500 mt-1 font-heading" v-if="showValidationError">This information is required</span>
         </div>
         <span class="text-xs text-red-500 mt-1 font-heading" v-if="showValidationError">This information is required</span>
     </div>
